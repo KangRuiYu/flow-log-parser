cb0b49ce93eb1ae0db36ac1603a530b65851dc81
KangRuiYu <karuiyu@gmail.com>
2024-08-31 15:13:28 -0700

    Initial commit

---
+++ flow_log_parser.py
@@ -0,0 +1,110 @@
+import argparse
+import pathlib
+import socket
+import re
+from collections import Counter
+
+
+TITLE = "Flow Log Parser"
+DESC = "Program that parses a file with flow log data to tags from a lookup file."
+LOG_REGEX = re.compile(r"^(\d+) \d+ .+ [0-9|\.]+ [0-9|\.]+ \d+ (\d+) (\d+) \d+ \d+ \d+ \d+ \w+ \w+$")
+LOG_VERSION = "2"
+
+
+def gen_protocol_mappings():
+    """Generate mapping from protocol numbers (in lower case) to their names using socket lib."""
+
+    mappings = {}
+
+    for name, value in vars(socket).items():
+        if name.startswith("IPPROTO_"):
+            mappings[str(value)] = name[8:].lower()
+
+    return mappings
+
+
+def parse_lookup(path):
+    """
+    Load the lookup file at [path], returning contents in a dictionary. Rows with missing
+    values are skipped.
+    """
+
+    lookup = {}
+
+    with path.open("r") as lookup_file:
+        for line in lookup_file:
+            if len(split_line := line.strip().split(",")) != 3:
+                continue
+
+            dstport, protocol, tag = split_line
+            lookup[f"{dstport},{protocol.lower()}"] = tag
+
+    return lookup
+
+
+def parse_log(path, lookup):
+    """
+    Parse each row of flow log file at [path] to tags using [lookup]. Rows with missing values are
+    skipped.
+    """
+
+    with path.open("r") as log_file:
+        tag_counts = Counter()
+        port_protocol_counts = Counter()
+        protocol_mappings = gen_protocol_mappings()
+
+        for line in log_file:
+            if not (line_match := LOG_REGEX.fullmatch(line.strip())):
+                continue
+            if line_match.group(1) != LOG_VERSION: # Check log version
+                continue
+            if not line_match.group(3) in protocol_mappings:
+                continue
+
+            _, dstport, protocol_num = line_match.groups()
+            key = f"{dstport},{protocol_mappings[protocol_num]}"
+
+            port_protocol_counts[key] += 1
+
+            if key in lookup:
+                tag_counts[lookup[key]] += 1
+            else:
+                tag_counts["Untagged"] += 1
+
+        return tag_counts, port_protocol_counts
+
+
+def write_output(path, tag_counts, port_protocol_counts):
+    """Write [tag_counts] and [port_protocol_counts] to output file at [path]."""
+
+    with path.open("w") as output_file:
+        output_file.write("Tag Counts:\nTag,Count\n")
+        for tag, count in tag_counts.items():
+            output_file.write(f"{tag},{count}\n")
+
+        output_file.write("Port/Protocol Combination Counts:\nPort,Protocol,Count\n")
+        for port_protocol, count in port_protocol_counts.items():
+            output_file.write(f"{port_protocol},{count}\n")
+
+
+def run(log_path, lookup_path, output_path):
+    """Run program on parse file [log_path] with table from [lookup_path], outputting to [output_path]."""
+
+    lookup = parse_lookup(lookup_path)
+    tag_counts, port_protocol_counts = parse_log(log_path, lookup)
+    write_output(output_path, tag_counts, port_protocol_counts)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog = TITLE,
+        description = DESC,
+    )
+
+    parser.add_argument("log_path", type=pathlib.Path)
+    parser.add_argument("lookup_path", type=pathlib.Path)
+    parser.add_argument("output_path", type=pathlib.Path)
+
+    args = parser.parse_args()
+    run(args.log_path, args.lookup_path, args.output_path)
+    print(f"Wrote to {args.output_path.absolute()}")
---
+++ test_flow_log_parser.py
@@ -0,0 +1,134 @@
+import unittest
+import flow_log_parser as parser
+from pathlib import Path
+
+
+class TestGenProtocolMappings(unittest.TestCase):
+    def setUp(self):
+        self.mappings = parser.gen_protocol_mappings()
+
+    def test_udp(self):
+        self.assertTrue("17" in self.mappings)
+        self.assertEqual(self.mappings["17"], "udp")
+
+    def test_tcp(self):
+        self.assertTrue("6" in self.mappings)
+        self.assertEqual(self.mappings["6"], "tcp")
+
+    def test_icmp(self):
+        self.assertTrue("1" in self.mappings)
+        self.assertEqual(self.mappings["1"], "icmp")
+
+
+class TestParseLookup(unittest.TestCase):
+    def test_empty(self):
+        path = Path("test_inputs/empty.txt")
+        self.assertEqual(parser.parse_lookup(path), {})
+
+    def test_invalid_path(self):
+        path = Path("test_inputs/asdopfiuqwepo.txt")
+        self.assertRaises(FileNotFoundError, parser.parse_lookup, path)
+
+    def test_sample(self):
+        path = Path("test_inputs/sample_lookup.txt")
+        self.assertEqual(parser.parse_lookup(path), {
+            "25,tcp": "sv_P1",
+            "68,udp": "sv_P2",
+            "23,tcp": "sv_P1",
+            "31,udp": "SV_P3",
+            "443,tcp": "sv_P2",
+            "22,tcp": "sv_P4",
+            "3389,tcp": "sv_P5",
+            "0,icmp": "sv_P5",
+            "110,tcp": "email",
+            "993,tcp": "email",
+            "143,tcp": "email"
+        })
+
+    def test_invalid_lookup(self):
+        path = Path("test_inputs/invalid_lookup.txt")
+        self.assertEqual(parser.parse_lookup(path), {
+            "25,tcp": "sv_P1",
+            "443,tcp": "sv_P2",
+            "22,tcp": "sv_P4",
+            "3389,tcp": "sv_P5",
+            "0,icmp": "sv_P5",
+            "110,tcp": "email",
+            "993,tcp": "email",
+            "143,tcp": "email"
+        })
+
+    def test_single_lookup(self):
+        path = Path("test_inputs/single_lookup.txt")
+        self.assertEqual(parser.parse_lookup(path), {"31,udp": "SV_P3"})
+
+
+class TestParseLog(unittest.TestCase):
+    def setUp(self):
+        path = Path("test_inputs/sample_lookup.txt")
+        self.lookup = parser.parse_lookup(path)
+
+    def test_empty(self):
+        path = Path("test_inputs/empty.txt")
+        tag_counts, port_protocol_counts = parser.parse_log(path, self.lookup)
+        self.assertEqual(len(tag_counts), 0)
+        self.assertEqual(len(port_protocol_counts), 0)
+
+    def test_invalid_path(self):
+        path = Path("test_inputs/alkdjf.txt")
+        self.assertRaises(FileNotFoundError, parser.parse_log, path, self.lookup)
+
+    def test_sample(self):
+        path = Path("test_inputs/sample_log.txt")
+        tag_counts, port_protocol_counts = parser.parse_log(path, self.lookup)
+        self.assertEqual(tag_counts, {
+            "sv_P2": 1,
+            "sv_P1": 2,
+            "email": 3,
+            "Untagged": 8
+        })
+        self.assertEqual(port_protocol_counts, {
+            "49153,tcp": 1,
+            "49154,tcp": 1,
+            "49155,tcp": 1,
+            "49156,tcp": 1,
+            "49157,tcp": 1,
+            "49158,tcp": 1,
+            "80,tcp": 1,
+            "1024,tcp": 1,
+            "443,tcp": 1,
+            "23,tcp": 1,
+            "25,tcp": 1,
+            "110,tcp": 1,
+            "993,tcp": 1,
+            "143,tcp": 1
+        })
+
+    def test_invalid_log(self):
+        path = Path("test_inputs/invalid_log.txt")
+        tag_counts, port_protocol_counts = parser.parse_log(path, self.lookup)
+        self.assertEqual(tag_counts, {
+            "sv_P1": 1,
+            "email": 2,
+            "Untagged": 5
+        })
+        self.assertEqual(port_protocol_counts, {
+            "49154,tcp": 1,
+            "49156,tcp": 1,
+            "49157,tcp": 1,
+            "49158,tcp": 1,
+            "1024,tcp": 1,
+            "25,tcp": 1,
+            "110,tcp": 1,
+            "143,tcp": 1
+        })
+
+    def test_single_log(self):
+        path = Path("test_inputs/single_log.txt")
+        tag_counts, port_protocol_counts = parser.parse_log(path, self.lookup)
+        self.assertEqual(tag_counts, {"email": 1})
+        self.assertEqual(port_protocol_counts, {"993,tcp": 1})
+
+
+if __name__ == "__main__":
+    unittest.main()
